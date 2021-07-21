c3e16c6959bbac942ac338cdbef6b61b0654ad68
Zaky7 <zak.saifi7@gmail.com>
2021-07-21 23:48:06 +0530

    test: WeatherInfoServiceTest added

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,12 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive:2.5.1")
 
+	// Cassandra
+	implementation("org.springframework.boot:spring-boot-starter-data-cassandra-reactive")
+	implementation("org.cognitor.cassandra:cassandra-migration-spring-boot-starter:2.4.0_v4")
+	testImplementation("org.cassandraunit:cassandra-unit-spring:4.3.1.0")
+	testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/com/reactive/sse/services/WeatherInfoServiceTest.kt
@@ -0,0 +1,70 @@
+package com.reactive.sse.services
+
+import com.reactive.sse.common.ObjectMapper
+import com.reactive.sse.model.WeatherInfoEvent
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+import org.mockito.stubbing.OngoingStubbing
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.core.ReactiveValueOperations
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+
+internal class WeatherInfoServiceTest {
+
+    private val redisTemplate: ReactiveRedisTemplate<String, String> = mock()
+    private val valueOperations: ReactiveValueOperations<String, String> = mock()
+    private val objectMapper = ObjectMapper()
+    private val redisService = RedisService(redisTemplate, objectMapper)
+    private val weatherInfoService = WeatherInfoService(redisService)
+
+    private fun <T> whenever(methodCall: T): OngoingStubbing<T> = Mockito.`when`(methodCall)
+
+    @Test
+    fun `should not return weather info when key not present in the redis and the database`() {
+        // given
+        val emptyWeatherInfoKey = "EMPTY_TICKER"
+
+        whenever(redisTemplate.opsForValue()).thenReturn(valueOperations)
+        whenever(redisTemplate.opsForValue().get(emptyWeatherInfoKey)).thenReturn(Mono.empty())
+
+        // when
+        val weatherInfoListMono: Mono<List<WeatherInfoEvent>> =
+            weatherInfoService.getWeatherDataFromRedis(emptyWeatherInfoKey)
+
+        // then
+        StepVerifier.create(weatherInfoListMono)
+            .expectSubscription()
+            .verifyComplete()
+    }
+
+
+    @Test
+    fun `should return weather info event when present in the cache`() {
+        val weatherInfoKey = "today"
+        val expectedWeatherInfoEventList = listOf(WeatherInfoEvent(122, 89))
+
+        whenever(redisTemplate.opsForValue()).thenReturn(valueOperations)
+        whenever(
+            redisTemplate.opsForValue().get(weatherInfoKey)
+        ).thenReturn(Mono.just(objectMapper.serialize(expectedWeatherInfoEventList)))
+
+        // when
+        val weatherInfoListMono: Mono<List<WeatherInfoEvent>> =
+            weatherInfoService.getWeatherDataFromRedis(weatherInfoKey)
+
+        // then
+        StepVerifier.create(weatherInfoListMono)
+            .expectSubscription()
+            .assertNext { weatherInfoEvent: List<WeatherInfoEvent> ->
+                MatcherAssert.assertThat(weatherInfoEvent, Matchers.`is`(expectedWeatherInfoEventList))
+            }
+            .verifyComplete()
+
+    }
+}
+
